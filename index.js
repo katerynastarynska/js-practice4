ad8924c5488b8407b54bc7e8f9514e02fb88765a
katerynastarynska <katerynastarynska@gmail.com>
2022-09-28 11:57:54 -0500

    js cards

---
+++ index.js
@@ -0,0 +1,31 @@
+import { productItems } from './products-items.js';
+
+const products = document.querySelector('.row');
+
+const productsMarkUp = productItems.map((product) => {
+    products.insertAdjacentHTML('beforeEnd',
+        `<div class="card" style="width: 18rem;">
+            <img src="${product.image}" class="card-img-top" alt="${product.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${product.name}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-price">$${product.price}</p>
+                    <btn class="btn btn-primary">${product.quantity} in stock</btn>
+                </div>
+        </div>`
+    )
+}).join(" ");
+
+const buttons = document.querySelectorAll("btn");
+
+buttons.forEach(button => button.addEventListener('click',
+    function onButtonClick(event) {
+        const itemQuantity = event.currentTarget.innerText;
+        const result = parseInt(itemQuantity.split(' ')[0]) - 1;
+        event.currentTarget.innerText = `${result} in stock`;
+
+        if (result <= 0) {
+            button.classList.add('disabled');
+        }
+    }
+))
---
+++ products-items.js
@@ -0,0 +1,56 @@
+export const productItems = [
+    {
+        image: 'https://cdn.pixabay.com/photo/2016/11/18/17/20/colorful-1835921_960_720.jpg',
+        name: 'colorful macaroons',
+        price: 10.99,
+        quantity: 35,
+    },
+    {
+        image: 'https://cdn.pixabay.com/photo/2019/03/13/15/31/macarons-4053115_960_720.jpg',
+        name: 'pastel macaroons',
+        price: 9.99,
+        quantity: 38,
+    },
+    {
+        image: 'https://cdn.pixabay.com/photo/2020/01/05/07/15/colorful-4742428_960_720.jpg',
+        name: 'rainbow macaroons',
+        price: 25.99,
+        quantity: 39,
+    },
+    {
+        image: 'https://cdn.pixabay.com/photo/2017/07/28/14/26/macarons-2548814_960_720.jpg',
+        name: 'purple macaroons',
+        price: 6.99,
+        quantity: 25,
+    },
+    {
+        image: 'https://cdn.pixabay.com/photo/2019/03/13/21/16/cookies-4053771_960_720.jpg',
+        name: 'beige macaroons',
+        price: 5.99,
+        quantity: 6,
+    },
+    {
+        image: 'https://cdn.pixabay.com/photo/2017/07/28/14/28/macarons-2548818_960_720.jpg',
+        name: 'raspberry macaroons',
+        price: 8.99,
+        quantity: 15,
+    },
+    {
+        image: 'https://cdn.pixabay.com/photo/2020/02/19/20/14/macarons-4863161_960_720.jpg',
+        name: 'easter macaroons',
+        price: 10.99,
+        quantity: 35,
+    },
+    {
+        image: 'https://cdn.pixabay.com/photo/2020/01/09/21/45/valentines-day-4754008_960_720.jpg',
+        name: 'heart macaroons',
+        price: 15.99,
+        quantity: 75,
+    },
+    {
+        image: 'https://cdn.pixabay.com/photo/2016/01/20/04/40/pink-macaroons-1150885_960_720.jpg',
+        name: 'pink macaroons',
+        price: 11.99,
+        quantity: 32,
+    },
+]
